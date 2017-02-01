5a98d4d5b384721b6a249b50782126b20b907358
Daley, Derek <Derek_Daley@csx.com>
2017-01-31 22:12:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Install with `npm install prep-cordova`
+
+## API
+Use prep-cordova to consume array of plugins in package.json. Must be ran
+in the directory where the package.json is located.
+
+Example:
+```
+"cordovaPlugins": [
+    "cordova-plugin-mfp",
+    "cordova-plugin-mfp-jsonstore",
+    "cordova-plugin-barcodescanner"
+  ]
+```
+
+## Optional
+Flag of --mfp can be set to true or false.
+True makes it compatible with the IBM MobileFirst CLI
+Defaults to false
+
+Flag of --platforms can be set to true or false.
+True makes it generate platforms located in config.xml
+Defaults to true
+
+Example:
+```
+prep-cordva --mfp=true
+```
---
+++ main.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var xml2js = require('xml2js');
+var exec = require('child_process').execSync;
+var pjson = require(process.cwd()+'/package.json');
+var configxml = process.cwd()+'/config.xml';
+var plugins = pjson.cordovaPlugins;
+var parser = new xml2js.Parser();
+var prefix = "";
+var needPlatform = true;
+
+prefix = (process.argv[2] == '--mfp=true' || 
+    process.argv[3] == '--mfp=true')? "mfp " : "";
+
+needPlatform = (process.argv[2] == '--platforms=true' ||
+    process.argv[3] == '--platforms=true');
+
+var installPlatforms = function(callback){
+    fs.readFile(configxml, function(err, data) {
+        parser.parseString(data, function (err, result) {
+            result.widget.platform.forEach(function(value){
+                var cmd = prefix + "cordova platform add "+ value.$.name;
+                exec(cmd, {stdio: 'inherit'});
+            });
+            setTimeout(function() {
+                callback();
+            }, 200);
+        });
+    });
+}
+
+var installPlugins = function(){
+    plugins.forEach(function(value){
+        var cmd = prefix + "cordova plugin add "+ value;
+        exec(cmd, {stdio: 'inherit'});
+    });
+}
+
+if(needPlatform){
+    installPlatforms(function(){installPlugins();});
+}else{
+    installPlugins();
+}
